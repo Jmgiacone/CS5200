b9028847a203a10471af85a78cd039197e8b0300
Jordan Giacone <Jmgiacone@gmail.com>
2017-03-16 18:43:37 -0500

    Shell of hw6 part 2 - magic squares

---
+++ src/MagicSquares.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+
+def generate_magic_square(n):
+    # 3 cases:
+    # 1: n is odd
+    # 2: n is singly even (divisible by 2 but not by 4 -> 4n + 2)
+    # n is doubly even (divisible by 2 and 4 -> 4n)
+    pass
+
+if __name__ == "__main__":
+    main()
